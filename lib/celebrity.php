493a41499ee598268ec33113c837dc7b989ecbd1
Franckyroquai <franckyroquai@gmail.com>
2024-03-07 18:16:47 +0100

    structure html celebrity

--- lib/celebrity.php
+++ lib/celebrity.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Celebrity</title>
+    <link rel="stylesheet" href="../css/style-celebrity.css">
 </head>
 <body>
     <div class="celebrity" >
         <h1>Les Célébrités !</h1>
         <div class="btn-ajout" >
             <a href="../">Ajouter une célébrité</a>
-
         </div>
+        <img src="../images/celebrity-1.jpg" alt="">
     </div>
     
     
