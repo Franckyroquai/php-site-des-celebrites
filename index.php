2fd876fde2350d6562038782186238dff48eea3e
Franckyroquai <franckyroquai@gmail.com>
2024-03-07 18:16:31 +0100

    structure html accueil

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceuil des Célébrités</title>
+    <link rel="stylesheet" href="css/style-accueil.css">
 </head>
 <body>
     <h1>Je choisis mes Célébrités !!!</h1>
