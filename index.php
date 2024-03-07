2a686c2eb3d1bfa0cbd094ecef6821011656176e
Franckyroquai <franckyroquai@gmail.com>
2024-03-07 15:33:40 +0100

    structure html de l'accueil

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>Acceuil des Célébrités</title>
 </head>
 <body>
+    <h1>Je choisis mes Célébrités !!!</h1>
+    <form action="index.html" method="post" enctype="multipart/form-data" >
+    <input type="hidden" name="MAX_FILE_SIZE" value="2097152" >
+    <input type="file" name="image" >
+    <input type="submit" name="Ajouter" >
+    </form>
+    <div class="btn-affiche" >
+        <a href="lib/celebrity.php">Afficher les Célébrités !</a>
+    </div>
     
 </body>
 </html>
