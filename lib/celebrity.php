068aa37caeb794dc989565ffaa6ed29679d49140
Franckyroquai <franckyroquai@gmail.com>
2024-03-08 17:31:46 +0100

    ajout de boutons ajout et reinitialisation

--- lib/celebrity.php
+++ lib/celebrity.php
@@ -1,3 +1,5 @@
+
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -16,10 +18,43 @@
       //var_dump($_FILES);
 
       if (isset($_FILES["image"]["name"]) && ($_FILES["image"]["error"] ==0)) {
-        var_dump($_FILES);
-    }
+        //stockage de l'image
+        if(!isset($_SESSION["adresse"])){
+          $nom_dossier_visiteur = time();
+          mkdir("../images/".$nom_dossier_visiteur);
+          $_SESSION["chemin"] = "../images/".$nom_dossier_visiteur;
+        }
 
+        $dossier_destination_fichier = $_SESSION["chemin"]."/".$_FILES["image"]["name"];
+
+        move_uploaded_file($_FILES["image"]["tmp_name"], $dossier_destination_fichier);
+        $_SESSION["adresse"][$_FILES["image"]["name"]] = $dossier_destination_fichier;
+
+      }
+
+      //Affichage de l'image
+      if (isset($_SESSION["adresse"])){
+        foreach ($_SESSION["adresse"] as $key => $value) {
+          echo "<img src=\"$value\" alt=\"$key\" />";
+        }
+      }
       ?>
+
+        <div class="btn-ajout" >
+          <?php
+          if (isset($_SESSION["adresse"])){
+            echo '<a href="../">Ajouter une célébrité</a>';
+          }
+          ?>
+            
+        </div>
+        <div class="btn-ajout" >
+        <?php
+          if (isset($_SESSION["adresse"])){
+            echo '<a href="logout.php">Réinitialiser</a>';
+          }
+          ?>
+        </div>
 
 
     </div>
